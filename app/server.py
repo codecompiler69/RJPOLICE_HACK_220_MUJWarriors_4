0dd8a2c521d6b1a38c00103c9d91fe2577d38b37
codecompiler69 <tosarthak07@gmail.com>
2023-12-24 19:42:53 +0530

    updated server to translate text from hin to eng

--- app/server.py
+++ app/server.py
@@ -1,8 +1,16 @@
 from flask import Flask, request, jsonify
 from PIL import Image
 import pytesseract
+from googletrans import Translator
 
 app = Flask(__name__)
+translator = Translator()
+
+def divide_text(text, chunk_size=500):
+    """
+    Divide the input text into chunks of specified size.
+    """
+    return [text[i:i+chunk_size] for i in range(0, len(text), chunk_size)]
 
 @app.route('/detect_text', methods=['POST'])
 def detect_text():
@@ -15,7 +23,16 @@ def detect_text():
     # Use pytesseract to extract text from the image
     text = pytesseract.image_to_string(Image.open(image), lang='eng+hin')
 
-    return jsonify({'text': text})
+    # Divide the text into chunks
+    text_chunks = divide_text(text)
+
+    # Translate each chunk and concatenate the results
+    translated_text = ''
+    for chunk in text_chunks:
+        translated_chunk = translator.translate(chunk, dest='english').text
+        translated_text += translated_chunk + ' '
+
+    return jsonify({'text': text, 'translated_text': translated_text})
 
 if __name__ == '__main__':
-    app.run(host='192.168.1.7',debug=True)
+    app.run(host='192.168.1.7', debug=True)
